924abf23eadb0a8956753e44e04bb842968394e1
jj-diaz1067 <juanjose21@live.com.co>
2015-03-25 23:03:15 -0500

    Los metodos GET de la interfaz estan implementados

--- HK.interfaz/public_html/js/Hospital.js
+++ HK.interfaz/public_html/js/Hospital.js
@@ -1,7 +1,6 @@
 (function () {
     var hospitalKennedy = angular.module('hospitalKennedy', []);
     var pacienteActual;
-
     hospitalKennedy.directive('toolbar', function () {
         return{
             restrict: 'E',
@@ -35,7 +34,6 @@
             controllerAs: 'getPacientes'
         };
     });
-
     hospitalKennedy.directive('nuevoPaciente', function () {
         return{
             restrict: 'E',
@@ -58,7 +56,6 @@
             controllerAs: 'agregarPaciente'
         };
     });
-
     hospitalKennedy.directive('reportes', function () {
         return{
             restrict: 'E',
@@ -67,14 +64,14 @@
                     var self = this;
                     console.log("Se busca los reportes del señor: " + idPaciente);
                     self.reportesPacientes = [];
+                    self.reporte= {};
                     this.darReportesDeId = function (id) {
                         console.log("Busca los reportes del id " + id);
                         //console.log('http://localhost:8080/hospitalKennedy.servicios/webresources/Pacientes/'+id+'/reportes/');
-                        self.reportesPacientes = [{id: '12345678', actividadFisica: 'Yoloismo', alimentacion: "Carne", gravedad: "gravisima", fechaCreacion: "2015/04/03", localizacionDolor: "detras de la cabeza", patronSuenio: "poco sueño", medicamentosRecientes: "Vicodin"}, {id: '12345678', actividadFisica: 'Yoloismo', alimentacion: "Carne", gravedad: "gravisima", fechaCreacion: "2015/04/03", localizacionDolor: "detras de la cabeza", patronSuenio: "poco sueño", medicamentosRecientes: "Vicodin"}];
+                        self.reportesPacientes = [{id: '87654321', actividadFisica: 'Yoloismo', alimentacion: "Carne", gravedad: "gravisima", fechaCreacion: "2015/04/03", localizacionDolor: "detras de la cabeza", patronSuenio: "poco sueño", medicamentosRecientes: "Vicodin"}, {id: '12345678', actividadFisica: 'Yoloismo', alimentacion: "Carne", gravedad: "gravisima", fechaCreacion: "2015/04/03", localizacionDolor: "detras de la cabeza", patronSuenio: "poco sueño", medicamentosRecientes: "Vicodin"}];
                         $http.get('http://localhost:8080/hospitalKennedy.servicios/webresources/Pacientes/' + id + '/reportes/').success(function (data) {
                             self.reportesPacientes = data;
                         });
-
                         return self.reportesPacientes;
                     };
                     this.darReportesEntreFechas = function (id, fecha1, fecha2) {
@@ -83,16 +80,22 @@
                         $http.get('http://localhost:8080/hospitalKennedy.servicios/webresources/Pacientes/' + id + '/reportes/' + fecha1 + '/' + fecha2).success(function (data) {
                             self.reportesPacientes = data;
                         });
-
                         return self.reportesPacientes;
                     };
-
-
+                    this.darDetalles = function (idPaciente, idReporte) {
+                        console.log('entroo a dar detalles del paciente: ' + idPaciente + ' sobre el reporte: ' + idReporte);
+                        $http.get('http://localhost:8080/hospitalKennedy.servicios/webresources/Pacientes/' + idPaciente + '/reportes/' + idReporte).success(function (data) {
+                            self.reporte = data;
+                        });
+                        self.reportesPacientes = [{id: '12345678', actividadFisica: 'Yoloismo', alimentacion: "Carne", gravedad: "gravisima", fechaCreacion: "2015/04/03", localizacionDolor: "detras de la cabeza", patronSuenio: "poco sueño", medicamentosRecientes: "Vicodin"}];
+                    };
+                    this.darReporte = function () {
+                        return self.reporte;  
+                    };
                 }],
             controllerAs: 'getReportes'
         };
     });
-
     hospitalKennedy.controller('pacientesActuales', function () {
         var pacientes;
         console.log("Entro al metodo, al menos");
@@ -108,7 +111,6 @@
             return pacienteActual;
         };
     });
-
     hospitalKennedy.directive('reporteForm', function () {
         return{
             restrict: 'E',
@@ -126,5 +128,19 @@
             controllerAs: 'reporteCtrl'
         };
     });
-
+    hospitalKennedy.directive('detallesReporte', function () {
+        return{
+            restrict: 'E',
+            templateUrl: 'partials/detallesReporte.html',
+            controller: ['$http', function ($http) {
+                    var self = this;
+                    self.reporte = [];
+                    self.reporte = [{id: '12345678', actividadFisica: 'Yoloismo', alimentacion: "Carne", gravedad: "gravisima", fechaCreacion: "2015/04/03", localizacionDolor: "detras de la cabeza", patronSuenio: "poco sueño", medicamentosRecientes: "Vicodin"}];
+                    self.reporte = hospitalKennedy.getReportes.darReporte;
+                    console.log('El reporte a dar detalles es '+ self.reporte);
+                    
+                }],
+            controllerAs: 'detallesReporte'
+        };
+    });
 })();
