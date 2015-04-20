a8a2d3aa7a9d2822a4e699bb13b7b63a5677c036
jssalamanca1967 <js.salamanca1967@uniandes.edu.co>
2015-04-20 13:46:38 -0500

    Implementacion seguridad Shiro

---
+++ hospitalKennedy.security/src/main/java/co/edu/uniandes/hospitalkennedy/security/logic/dto/PathInfo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.hospitalkennedy.security.logic.dto;
+
+import com.stormpath.sdk.group.Group;
+
+/**
+ *
+ * @author estudiante
+ */
+public class PathInfo {
+    
+    public final static String ADMIN = "Admin";
+    public final static String DOCTOR = "Doctor";
+    public final static String PACIENTE = "Paciente";
+    
+    public final static String PATH_DOCTOR = "Doctor";
+    public final static String PATH_PACIENTE = "Pacientes";
+    public final static String PATH_ADMIN = "Admin";
+    
+    public static String pathInfo;
+    
+    public static boolean autenticar(String grupo){
+        
+        boolean rta = false;
+       
+        return rta;
+    }
+    
+}
